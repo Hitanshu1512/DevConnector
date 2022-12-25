5d86c6cb8e36fe69c5e1c83268d6eed9d38b46c9
Hitanshu1512 <hitanshu1512@gmail.com>
2022-12-25 11:27:15 +0530

    Encrypted password

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/User");
@@ -29,22 +31,40 @@ router.post(
     const { name, email, password } = req.body;
 
     try {
-
       // see if the user exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ error: [{ msg: "User already exists!" }] });
+      }
 
       // Get users gravator
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
 
       // Encrypt password
+      const salt = await bcrypt.genSalt(10);    // salt is used for hashing purpose
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
 
       // Return jsonwebtokens
-
-      res.send("User Route");
-
+      res.send("User Registered!");
     } catch (err) {
-
       console.error(err.message);
-      res.status(500).send('Server Error!');
-    }
+      res.status(200).send("Server Error !");
+    } 
   }
 );
 
