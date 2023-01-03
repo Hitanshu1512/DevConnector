650d2a534dfb0507190bbd34204fec8502a4434f
Hitanshu1512 <hitanshu1512@gmail.com>
2023-01-03 15:55:59 +0530

    ready to deploy

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,7 @@ const { check, validationResult } = require("express-validator");
 
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
+const Post = require("../../models/Post");
 
 // @route    GET api/profile/me
 // @desc     Test route
@@ -159,7 +160,7 @@ router.get("/user/:user_id", async (req, res) => {
 router.delete("/", auth, async (req, res) => {
   try {
     // remove user's posts
-    // tooooodoooo
+    await Post.deleteMany({user: req.user.id});
 
     // remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
